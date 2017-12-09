a6fa1eb5d83078606537948195a831acda130d4c
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2017-12-09 15:16:02 +0530

    ICFJ-181 Adds polyfill for element.closest method on the data page.

--- src/js/App.js
+++ src/js/App.js
@@ -72,6 +72,20 @@ class App extends React.Component {
       $('.banner-area .sticky-wrapper').css("display", 'inline-block');
     }
 
+    //Polyfill for element.closest in old browsers.
+    if (window.Element && !Element.prototype.closest) {
+      Element.prototype.closest =
+        function (s) {
+          var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+            i,
+            el = this;
+          do {
+            i = matches.length;
+            while (--i >= 0 && matches.item(i) !== el) { };
+          } while ((i < 0) && (el = el.parentElement));
+          return el;
+        };
+    }
   }
 
   componentDidUpdate() {
